f87df2651f43eaa61ef80553df91f4cd4d00b5aa
Billy.Zheng <zw963@163.com>
2017-07-16 01:27:12 +0800

    webpacker 测试成功.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def helloworld
+
+  end
+end
---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test '#helloworld' do
+
+  end
+end
